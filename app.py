03ee3e8d28cfb2af2eb47108d775f7cc0d6d7c9f
Jannik Rosendahl <me@rosendahl.dev>
2025-05-13 10:34:03 +0200

    combine map and slider, layout

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 
+app = Dash()
+
+
 def load_data() -> pd.DataFrame:
     file_name = 'Europe-Central-Asia_2018-2025_May02.csv'
     data_path = 'data/' + file_name
@@ -14,58 +17,116 @@ def load_data() -> pd.DataFrame:
     # check if the file exists locally
     try:
         with open(data_path, 'r') as f:
-            df = pd.read_csv(f)
+            data = pd.read_csv(f)
             print('Loaded data from local file')
     except FileNotFoundError:
         os.makedirs('data', exist_ok=True)
         print('Local file not found, downloading from URL, this may take a minute')
-        df = pd.read_csv(url)
-        df.to_csv(data_path, index=False)
+        data = pd.read_csv(url)
+        data.to_csv(data_path, index=False)
         print('Downloaded data from URL and saved to local file')
-    return df
+    
+    # data preprocessing
+    data = data[data['actor1'].str.contains('ukraine|russia', case=False, na=False)]
+    data['event_date'] = pd.to_datetime(data['event_date'])
+    return data
 
 data = load_data()
 
-app = Dash()
 
-# Load data into application
-df = pd.read_csv('./data/Ukraine_Black_Sea_2020_2025_May02.csv', sep=',')
-
-minTimestamp = int(pd.Timestamp(pd.to_datetime(df['event_date']).min().date()).timestamp())
-maxTimestamp = int(pd.Timestamp(pd.to_datetime(df['event_date']).max().date()).timestamp())
+minTimestamp = int(pd.Timestamp(pd.to_datetime(data['event_date']).min().date()).timestamp())
+maxTimestamp = int(pd.Timestamp(pd.to_datetime(data['event_date']).max().date()).timestamp())
 
 selectedMinDate = minTimestamp
 selectedMaxDate = maxTimestamp
 
-df['event_date'] = pd.to_datetime(df['event_date'])
-df_filtered = df[(df['event_date'].apply(lambda x: int(pd.Timestamp(x).timestamp())) >= selectedMinDate) &
-                 (df['event_date'].apply(lambda x: int(pd.Timestamp(x).timestamp())) <= selectedMaxDate)]
+data_filtered = data[(data['event_date'].apply(lambda x: int(pd.Timestamp(x).timestamp())) >= selectedMinDate) &
+                 (data['event_date'].apply(lambda x: int(pd.Timestamp(x).timestamp())) <= selectedMaxDate)]
 
-first_of_years = df.groupby([df['event_date'].dt.year])['event_date'].min().sort_values()
+first_of_years = data.groupby([data['event_date'].dt.year])['event_date'].min().sort_values()
 
-app.layout = html.Div(children=[
-    html.H1(children='Ukraine Dashboard'),
+def create_map(minTimestamp=minTimestamp, maxTimestamp=maxTimestamp):
+    fig = px.scatter_map(
+        data[['latitude', 'longitude']],
+        lat='latitude',
+        lon='longitude',
+        #hover_name='name',
+        #hover_data=['lat', 'lon'],
+        color_discrete_sequence=['blue'],
+        zoom=10,
+        #height=600
+    )
+    fig.update_layout(mapbox_style='open-street-map')
+    fig.update_traces(marker=dict(size=5))
+    return fig
 
-    html.Div([
-        dcc.RangeSlider(
-            minTimestamp, maxTimestamp, 86400,  # 86400 seconds = 1 day
-            value=[minTimestamp, maxTimestamp],
-            id='date-slider',
-            marks={int(pd.Timestamp(date).timestamp()): date.strftime('%Y-%m-%d') for date in first_of_years},
-            tooltip={
-                "placement": "bottom", 
-                "always_visible": True,
-                "transform": "formatTimestamp"
-            }
+app.layout = html.Div(
+    style={
+        'display': 'grid',
+        'gridTemplateColumns': '2fr 1fr',
+        'gridTemplateRows': '2fr 1fr',
+        'gap': '2px',
+        'height': '100vh',
+        'padding': '2px',
+        'boxSizing': 'border-box',
+    },
+    children=[
+        html.Div(
+            children=[
+                html.Div(
+                    dcc.Graph(figure=create_map(), style={'height': '90%', 'width': '100%'}),
+                    style={'border': '1px solid #ccc', }
+                ),
+                html.Div(
+                    children=[
+                        dcc.RangeSlider(
+                            minTimestamp, maxTimestamp, 86400,  # 86400 seconds = 1 day
+                            value=[minTimestamp, maxTimestamp],
+                            id='date-slider',
+                            marks={int(pd.Timestamp(date).timestamp()): date.strftime('%Y-%m-%d') for date in first_of_years},
+                            tooltip={
+                                "placement": "bottom", 
+                                "always_visible": True,
+                                "transform": "formatTimestamp"
+                            }
+                        ),
+                        html.Div(id='date-slider-output')
+                    ]
+                )
+            ]
         ),
+        html.Div('Placeholder 2', style={
+            'backgroundColor': '#f0f0f0',
+            'display': 'flex',
+            'alignItems': 'center',
+            'justifyContent': 'center',
+            'border': '2px dashed #aaa',
+            'fontSize': '20px'
+        }),
+        html.Div('Placeholder 3', style={
+            'backgroundColor': '#f0f0f0',
+            'display': 'flex',
+            'alignItems': 'center',
+            'justifyContent': 'center',
+            'border': '2px dashed #aaa',
+            'fontSize': '20px'
+        }),
+        html.Div('Placeholder 4', style={
+            'backgroundColor': '#f0f0f0',
+            'display': 'flex',
+            'alignItems': 'center',
+            'justifyContent': 'center',
+            'border': '2px dashed #aaa',
+            'fontSize': '20px'
+        }),
+    ]
+)
 
-        html.Div(id='date-slider-output')
-    ])
-]) 
 
 @callback(
     Output('date-slider-output', 'children'),
-    Input('date-slider', 'value'))
+    Input('date-slider', 'value')
+)
 def update_output(value):
     start_date = pd.to_datetime(value[0], unit='s').strftime('%Y-%m-%d')
     end_date = pd.to_datetime(value[1], unit='s').strftime('%Y-%m-%d')
